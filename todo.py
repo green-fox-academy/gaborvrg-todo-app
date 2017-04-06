3f088315117437cf85babeba403e50981b7a843e
Gabor V <gabor.w.varga@gmail.com>
2017-04-06 14:24:53 +0200

    added help.txt and

--- todo.py
+++ todo.py
@@ -1,6 +1,32 @@
-class ClassName(object):
-    """docstring for ClassName"""
-    def __init__(self, arg):
-        super(ClassName, self).__init__()
-        self.arg = arg
-        
+import sys
+
+
+def arg_reader():
+
+    if len(sys.argv) == 1:
+        file = open('help.txt','r')
+        helptxt = file.read()
+        return helptxt
+
+    else:
+        return sys.argv[1:]
+
+
+
+
+
+arguments = arg_reader()
+print(arguments)
+
+# if len(arguments) == 0:
+#     print('help text')
+
+# elif:
+#     if( arguments[0] == '-l' ):
+#         print('Addolunk ilyet', arguments[1])
+# elif:
+#     if( arguments[0] == '-a' ):
+#         print('Addolunk ilyet', arguments[1])
+# else:
+#     if( arguments[0] == '-c' ):
+#         print('Addolunk ilyet', arguments[1])
