66d012b6f8df75da166e87b9f9377ab449d1f93f
Gabor V <gabor.w.varga@gmail.com>
2017-04-06 15:55:19 +0200

    added add_line and todo_list functions, create view modul(function)

--- todo.py
+++ todo.py
@@ -1,32 +1,52 @@
 import sys
 
 
+def help_txt():
+
+    file = open('help.txt','r')
+    helptxt = file.read()
+    print(helptxt)
+
+
 def arg_reader():
 
     if len(sys.argv) == 1:
-        file = open('help.txt','r')
-        helptxt = file.read()
-        return helptxt
-
+        help_txt()
     else:
-        return sys.argv[1:]
+        # return sys.argv[1:]
+        if ( sys.argv[1] == '-l' ):
+            todo_db()
+        elif ( sys.argv[1] == '-a' ):
+            add_line()
+
+
+def todo_db():
+    file = open('todo-db.txt','r')
+    todo_db = file.read()
+    print(todo_db)
+    file.close()
+
+
+def add_line():
+    # print(sys.argv[2])
+
+    file = open('todo-db.txt','a') 
+    file.write("\n0;" + sys.argv[2].rstrip())
+    file.close()
+    view()
+
+
+def view():
+    file = open('todo-db.txt','r')
+    view_file = file.readline().rstrip()
+    view_list = view_file.split(';')
+    file.close()
+    print(view_list)
+
+
+
+arg_reader()
 
 
 
 
-
-arguments = arg_reader()
-print(arguments)
-
-# if len(arguments) == 0:
-#     print('help text')
-
-# elif:
-#     if( arguments[0] == '-l' ):
-#         print('Addolunk ilyet', arguments[1])
-# elif:
-#     if( arguments[0] == '-a' ):
-#         print('Addolunk ilyet', arguments[1])
-# else:
-#     if( arguments[0] == '-c' ):
-#         print('Addolunk ilyet', arguments[1])
